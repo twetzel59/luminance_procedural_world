234496ad173c51752a0ba187b563c3b587796d86
twetzel59 <twetzel59@gmail.com>
2017-12-29 11:52:44 -0500

    Add new worldgen improvements. * Now using BasicMulti noise module. * New terrain formations.

--- src/terrain/world_gen.rs
+++ src/terrain/world_gen.rs
@@ -1,19 +1,21 @@
 //! Procedural world generation.
 
-use noise::{NoiseModule, Perlin};
+use noise::{BasicMulti, MultiFractal, NoiseModule};
 use super::SECTOR_SIZE;
-use super::voxel::{Block, BlockList};
+use super::voxel::{Block, BlockList, SectorSpaceCoords};
 
 /// The world generator.
 pub struct WorldGen {
-    perlin: Perlin,
+    //perlin: Perlin,
+    noisemod: BasicMulti<f32>,
 }
 
 impl WorldGen {
     /// Create a new `WorldGen`.
     pub fn new() -> WorldGen {
         WorldGen {
-            perlin: Perlin::new(),
+            //perlin: Perlin::new(),
+            noisemod: BasicMulti::new().set_persistence(0.1),
         }
     }
     
@@ -40,6 +42,7 @@ impl WorldGen {
         }
         */
         
+        /*
         if sector.1 > 0 {
             BlockList::new(
                     [Block::Air;
@@ -81,6 +84,36 @@ impl WorldGen {
             BlockList::new(
                     [Block::Limestone;
                     SECTOR_SIZE * SECTOR_SIZE * SECTOR_SIZE])
+        }
+        */
+        
+        if sector.1 == 0 {
+            let mut list = BlockList::new_air();
+            
+            for x in 0..SECTOR_SIZE {
+                for z in 0..SECTOR_SIZE {
+                    let height = self.noisemod.get(
+                        [(x as f32 + SECTOR_SIZE as f32 * sector.0 as f32) * 0.007,
+                         (z as f32 + SECTOR_SIZE as f32 * sector.2 as f32) * 0.007]);
+                    
+                    let middle = (SECTOR_SIZE / 2) as f32;
+                    
+                    let highest = (middle + height * 40.).max(0.).min(SECTOR_SIZE as f32) as usize;
+                    
+                    //println!("highest: {}", highest);
+                    
+                    for y in 0..highest {
+                        list.set(SectorSpaceCoords::new(x as u8, y as u8, z as u8),
+                                 Block::Grass);
+                    }
+                }
+            }
+            
+            list
+        } else if sector.1 == -1 {
+            BlockList::new([Block::Limestone; SECTOR_SIZE * SECTOR_SIZE * SECTOR_SIZE])
+        } else {
+            BlockList::new_air()
         }
     }
 }
